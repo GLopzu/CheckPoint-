6b4f0357be47504c77debc8defcf85b2bfc35bd3
LOPZU <116040988+GLopzu@users.noreply.github.com>
2023-09-01 19:38:36 -0500

    Charac.js
    add functional button + alternames

--- src/comp/charac/Charac.js
+++ src/comp/charac/Charac.js
@@ -1,58 +1,62 @@
 export class Chara extends HTMLElement {
-    selected = "seleccionado"
+  selected = this.charaName;
 
-    static get observedAttributes(){
-        return ["name", "specie", "gender", "house", "birth"]
+  static get observedAttributes() {
+    return ["name", "specie", "gender", "house", "birth", "alter"];
+  }
+
+  attributeChangedCallback(propName, _, newValue) {
+    switch (propName) {
+      case "name":
+        this.charaName = newValue;
+        break;
+      case "specie":
+        this.charaSpecie = newValue;
+        break;
+      case "gender":
+        this.charaGender = newValue;
+        break;
+      case "house":
+        this.charaHouse = newValue;
+        break;
+      case "birth":
+        this.charaBirth = newValue;
+        break;
+      case "alter":
+        this.charaAlter = newValue;
+      default:
+        break;
     }
+  }
 
-    attributeChangedCallback(propName, _, newValue){
-        switch (propName) {
-            case "name":
-                this.charaName = newValue;
-                break;
-            case "specie":
-                this.charaSpecie = newValue;
-                break;
-            case "gender":
-                this.charaGender = newValue;
-                break;
-            case "house":
-                this.charaHouse = newValue;
-                break;
-            case "birth":
-                this.charaBirth = newValue;
-                break;
-            default:
-                break;
-        }
-    }
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
 
-    constructor(){
-        super();
-        this.attachShadow({mode: "open"});
-    }
+  connectedCallback() {
+    this.mount();
+  }
 
-    connectedCallback(){
-        this.mount();
-    }
+  mount() {
+    this.render();
+    const btn = this.shadowRoot.querySelector("button");
+    btn.addEventListener("click", () => {
+      if (this.selected === this.charaAlter) {
+        this.selected = this.charaName;
+      } else {
+        if (this.charaAlter) {
+          this.selected = this.charaAlter;
+        }else {this.selected = "Sin apodo"}
+      }
 
-    mount(){
-        this.render();
-        const btn = this.shadowRoot.querySelector("button");
-        btn.addEventListener("click", () => {
-            if (this.selected === "no seleccionado"){
-                this.selected = "seleccionado";
-            } else {
-                this.selected = "No seleccionado";
-            }
-
-            this.mount();
-        });
-    }
-    render(){
-        this.shadowRoot.innerHTML = `
+      this.mount();
+    });
+  }
+  render() {
+    this.shadowRoot.innerHTML = `
         <section>
-        <h3>Nombre: ${this.charaName} </h3>
+        <h3>Nombre: ${this.selected} </h3>
         <h3>Specie: ${this.charaSpecie} </h3>
         <h3>Gender: ${this.charaGender} </h3>
         <h3>House: ${this.charaHouse} </h3>
@@ -60,7 +64,7 @@ export class Chara extends HTMLElement {
         <button> Click </button>
 
         </section>`;
-    }
+  }
 }
 
-customElements.define ("app-chara", Chara);
+customElements.define("app-chara", Chara);
